18b5a5b8c051ce1feafecdbede6c6169c572adf3
Victor Nova <victor@borg.games>
2025-05-11 18:06:03 -0700

    my: fixed tryGetExe

--- ts/gid.ts
+++ ts/gid.ts
@@ -3,8 +3,8 @@ const exePrefix = '//exe/';
 export class GameID {
     static tryGetExe(uriString: string) {
         const uri = new URL(uriString);
-        if (uri.protocol !== "borg:" || !uri.pathname.startsWith(exePrefix))
+        if (uri.protocol !== "borg:" || uri.host !== "exe" || !uri.pathname.startsWith('/'))
             return null;
-        return uri.pathname.substring(exePrefix.length);
+        return uri.pathname.substring(1);
     }
 }
